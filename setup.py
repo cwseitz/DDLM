95928653a1785121e49e3cb1b0ad6c44943de183
cwseitz <cwseitz2@gmail.com>
2023-11-14 21:40:45 -0500

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='qSMLM',
+    name='SPICE',
     version='0.0.1',
     description='',
     author='Clayton Seitz',
     author_email='cwseitz@iu.edu',
-    packages=['qSMLM'],
+    packages=['SPICE'],
 )
