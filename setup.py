80420a165ffe51cb8505859eb4b69cc274aa2576
clayton <cwseitz@iu.edu>
2024-03-02 21:03:34 -0500

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='OCI',
+    name='diffusion',
     version='0.0.1',
     description='',
     author='Clayton Seitz',
     author_email='cwseitz@iu.edu',
-    packages=['oci'],
+    packages=['diffusion'],
 )
